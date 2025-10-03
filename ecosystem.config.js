4839ad2b8b84b62cee5408a771487c0bc678f559
Henry Ugochukwu <phalcon.vee@gmail.com>
2025-10-03 10:02:12 -0700

    chore: update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,15 @@ export default {
       script: 'npm',
       args: 'start',
       cwd: '/var/www/prod',
+      env_file: '.env',
       env: {
         NODE_ENV: 'production',
         PORT: 8000
       },
       instances: 1,
-      exec_mode: 'fork'
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
     }]
 };
