590653fcf7b8528913637dc9c6873a739c116ff1
Sid <sid.aims93@gmail.com>
2025-11-08 18:20:39 +0530

    Finally logs removed

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -15,12 +15,6 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     },
   });
 
-  const allDbRecords = await prisma.session.findMany({
-    where: {
-      shop: shop
-    }
-  });
-
   if (!dbRecord?.shop || !dbRecord?.accessToken) {
     throw new Error("Missing session data");
   }
@@ -38,37 +32,23 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
   const jsonData = await response.json();
 
-  console.log('loader json data', jsonData);
-
   return {
     session,
     jsonData,
     shop: shop,
     accessToken: dbRecord.accessToken,
-    allDbRecords: allDbRecords
   };
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {};
 
 export default function Home() {
-  // const shopify = useAppBridge();
-  const {jsonData, allDbRecords} = useLoaderData<{
+  const { jsonData } = useLoaderData<{
     session: string;
     jsonData: any;
     accessToken: string;
     shop: string;
-    allDbRecords: any;
   }>();
-
-  console.log('response data', JSON.stringify(jsonData));
-  console.log('all db records', JSON.stringify(allDbRecords));
-
-  // Test API calls
-
-  // useEffect(() => {
-  //   shopify.toast.show("Product created");
-  // }, [shopify]);
 
   var dashboardUrl = "https://dashboard.fishook.online/merchant/auth/login";
   if(jsonData.hasOwnProperty('data') && jsonData.data.hasOwnProperty('jwt_token')) {
@@ -83,13 +63,6 @@ export default function Home() {
     <Page>
       <BlockStack gap="500">
         <Layout>
-          {/* {jsonData?.data ? (
-            <div>
-              <p>You are logged in now!</p>
-
-              <button onClick={widgetInstall}>Install Widget</button>
-            </div>
-          ) : ( */}
           <div className="fishook-auth-banner w-full min-h-60 rounded-2xl grid place-content-center pt-13 pb-8 mt-8">
             <div className="grid justify-center mb-5">
               <img
